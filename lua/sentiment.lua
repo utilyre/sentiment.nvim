feb7e3f609c88ffbaf8b6db1eb55078d79d33d57
Amirabbas Fazelinia <utilyre@proton.me>
2023-03-03 00:32:30 +0330

    feat: get the right pair working

--- lua/sentiment.lua
+++ lua/sentiment.lua
@@ -3,90 +3,64 @@ local Pair = require("sentiment.pair")
 
 local M = {}
 
+local function find_right(lefts, rights, top, bot, lines, row, col)
+  local start = col
+
+  local remaining = 0
+  for i = row, bot, 1 do
+    local line = lines[i]
+    if i == row + 1 then start = 1 end
+
+    for j = start, #line, 1 do
+      local char = line:sub(j, j)
+
+      if lefts[char] then
+        remaining = remaining + 1
+      elseif rights[char] then
+        if remaining == 0 then return { top + i - 1, j } end
+        remaining = remaining - 1
+      end
+    end
+  end
+end
+
 function M.setup()
   vim.g.loaded_matchparen = 1
 
-  local nsnr = vim.api.nvim_create_namespace("sentiment")
+  local ns = vim.api.nvim_create_namespace("sentiment")
+  local matchpairs = vim.opt.matchpairs:get()
+  local lefts = {}
+  local rights = {}
+  for _, matchpair in ipairs(matchpairs) do
+    local parts = vim.split(matchpair, ":", { plain = true })
+    lefts[parts[1]] = true
+    rights[parts[2]] = true
+  end
 
   vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
     group = vim.api.nvim_create_augroup("sentiment", {}),
-    callback = function(a)
-      if vim.bo[a.buf].buftype ~= "" then return end
+    callback = function(args)
+      args.win = vim.api.nvim_get_current_win()
+      if vim.bo[args.buf].buftype ~= "" then return end
 
-      local matchpairs = vim.opt.matchpairs:get()
-      local lefts = {}
-      local rights = {}
-      for _, matchpair in ipairs(matchpairs) do
-        local parts = vim.split(matchpair, ":", { plain = true })
-        table.insert(lefts, parts[1])
-        table.insert(rights, parts[2])
-      end
+      local top = vim.fn.line("w0", args.win)
+      local bot = vim.fn.line("w$", args.win)
 
-      local winnr = vim.api.nvim_get_current_win()
-      local cursor = vim.api.nvim_win_get_cursor(winnr)
+      local lines = vim.api.nvim_buf_get_lines(args.buf, top - 1, bot, true)
+      local row, col = unpack(vim.api.nvim_win_get_cursor(args.win))
+      col = col + 1
 
-      local current_line =
-        vim.api.nvim_buf_get_lines(a.buf, cursor[1] - 1, cursor[1], false)[1]
+      local left = nil
+      -- ...
+      left = { 1, 1 }
 
-      local left_half, right_half = utils.split_at(current_line, cursor[2] + 1)
-      -- TODO: fetch visible lines and lookup pairs in there
+      local right = find_right(lefts, rights, top, bot, lines, row, col)
 
-      -- BUG: highlighting breaks when cursor is ON a matchpair
-      -- Like () or ( { "Hi" }, "Hello" }
-      local left_pos, right_pos
+      vim.api.nvim_buf_clear_namespace(args.buf, ns, 0, -1)
+      if left == nil or right == nil then return end
 
-      local remaining_nests = 0
-      for i = #left_half, 1, -1 do
-        local ch = left_half:sub(i, i)
-
-        if vim.tbl_contains(rights, ch) then
-          remaining_nests = remaining_nests + 1
-          goto continue
-        end
-
-        if vim.tbl_contains(lefts, ch) then
-          if remaining_nests > 0 then
-            remaining_nests = remaining_nests - 1
-            goto continue
-          end
-
-          left_pos = i
-          break
-        end
-
-        ::continue::
-      end
-      --[[ local ]] remaining_nests = 0
-      for i = 1, #right_half, 1 do
-        local ch = right_half:sub(i, i)
-
-        if vim.tbl_contains(lefts, ch) then
-          remaining_nests = remaining_nests + 1
-          goto continue
-        end
-
-        if vim.tbl_contains(rights, ch) then
-          if remaining_nests > 0 then
-            remaining_nests = remaining_nests - 1
-            goto continue
-          end
-
-          right_pos = (#left_half - 1) + i
-          break
-        end
-
-        ::continue::
-      end
-
-      vim.api.nvim_buf_clear_namespace(a.buf, nsnr, 0, -1)
-      if left_pos ~= nil and right_pos ~= nil then
-        local pair = Pair.new(
-          { cursor[1] - 1, left_pos - 1 },
-          { cursor[1] - 1, right_pos - 1 }
-        )
-
-        if pair ~= nil then pair:draw(a.buf, nsnr) end
-      end
+      local pair = Pair.new({ 1, 1 }, right)
+      pair:draw(args.buf, ns)
     end,
   })
 end
