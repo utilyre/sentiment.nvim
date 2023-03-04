c430560a678f29c90f9daa99f4cfb36454809da9
Amirabbas Fazelinia <utilyre@proton.me>
2023-03-04 22:14:54 +0330

    perf(config): do a hashmap lookup instead of a forloop

--- lua/sentiment/config.lua
+++ lua/sentiment/config.lua
@@ -20,18 +20,23 @@ function M.is_buffer_included(buf)
   return cfg.included_buftypes[buftype] and not cfg.excluded_filetypes[filetype]
 end
 
+local left_matchpairs = {}
+local right_matchpairs = {}
+for _, matchpair in ipairs(cfg.matchpairs) do
+  left_matchpairs[matchpair[1]] = true
+  right_matchpairs[matchpair[2]] = true
+end
+
 ---Check whether a character is a matchpair.
 ---
 ---@param char string Character to be checked.
 ---@param direction "left"|"right" Direction of the matchpair.
----@return integer|nil
+---@return boolean
 function M.is_matchpair(char, direction)
-  for i, matchpair in ipairs(cfg.matchpairs) do
-    if direction == "left" and matchpair[1] == char then return i end
-    if direction == "right" and matchpair[2] == char then return i end
-  end
+  if direction == "left" then return left_matchpairs[char] end
+  if direction == "right" then return right_matchpairs[char] end
 
-  return nil
+  error(string.format("invalid direction: '%s'", direction), 2)
 end
 
 return M
