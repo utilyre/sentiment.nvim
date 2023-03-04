d59eb709f8b51091da79494a857cb821817fa2b3
Amirabbas Fazelinia <utilyre@proton.me>
2023-03-04 15:19:55 +0330

    feat(config): add included_buftypes and excluded_filetypes

--- lua/sentiment/config.lua
+++ lua/sentiment/config.lua
@@ -9,4 +9,15 @@ local cfg = default
 ---@param c Config
 function M.set(c) cfg = c end
 
+---Check whether a buffer is included.
+---
+---@param buf buffer Buffer to be checked.
+---@return boolean
+function M.is_buffer_included(buf)
+  local buftype = vim.bo[buf].buftype
+  local filetype = vim.bo[buf].filetype
+
+  return cfg.included_buftypes[buftype] and not cfg.excluded_filetypes[filetype]
+end
+
 return M
--- lua/sentiment/config/default.lua
+++ lua/sentiment/config/default.lua
@@ -1,5 +1,17 @@
 ---@class Config
 local Config = {
+  ---Dictionary to check whether a buftype should be included.
+  ---
+  ---@type table<string, boolean>
+  included_buftypes = {
+    [""] = true,
+  },
+
+  ---Dictionary to check whether a filetype should be excluded.
+  ---
+  ---@type table<string, boolean>
+  excluded_filetypes = {},
+
   ---List of `(left, right)` match pair tuples.
   ---
   ---@type tuple<string, string>[]
