b3e42762a133155e98b6d5b578c5056699d179d0
Amirabbas Fazelinia <utilyre@proton.me>
2023-03-05 00:10:42 +0330

    docs(autocmd): document create_updater()

--- lua/sentiment/autocmd.lua
+++ lua/sentiment/autocmd.lua
@@ -4,6 +4,7 @@ local GROUP_UPDATER = "sentiment.updater"
 
 local M = {}
 
+---Update pairs on necessary events.
 function M.create_updater()
   vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
     group = vim.api.nvim_create_augroup(GROUP_UPDATER, {}),
