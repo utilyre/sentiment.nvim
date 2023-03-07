c7b46647290cac91df2e4aa4177afda050dffeb8
Amirabbas Fazelinia <utilyre@proton.me>
2023-03-07 20:56:03 +0330

    refactor(autocmd): move AutocmdArgs into Autocmd module

--- lua/sentiment/Autocmd.lua
+++ lua/sentiment/Autocmd.lua
@@ -1,3 +1,12 @@
+---@class AutocmdArgs
+---@field public id integer Autocmd ID.
+---@field public group integer|nil Group ID.
+---@field public event string Name of the triggered event.
+---@field public match string Value that was matched against the pattern.
+---@field public buf buffer Buffer being manipulated.
+---@field public file string File name of the buffer being manipulated.
+---@field public data any Passed arbitrary data.
+
 ---@class Autocmd
 ---@field private id integer|nil Autocmd ID.
 ---@field public name string Name used for creating its augroup.
