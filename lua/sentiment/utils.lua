bfb79ea5521e707b7eb74e462e3cb1b1128b95c7
Amirabbas Fazelinia <utilyre@proton.me>
2023-03-14 22:55:51 +0330

    docs(utils): improve code docs

--- lua/sentiment/utils.lua
+++ lua/sentiment/utils.lua
@@ -2,10 +2,10 @@ local M = {}
 
 ---If `cond` is true, return `t` otherwise `f`.
 ---
----NOTE: One drawback is that both expression will be executed at runtime.
+---NOTE: The drawback is that both expression will be executed at runtime.
 ---
 ---@generic T, F
----@param cond boolean Condition expression.
+---@param cond boolean Condition pattern.
 ---@param t T Consequent expression.
 ---@param f F Alternative expression.
 ---@return T|F
