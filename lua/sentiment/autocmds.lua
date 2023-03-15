6a91aef058af347b1b157fe273bd4c6b3ede0644
Amirabbas Fazelinia <utilyre@proton.me>
2023-03-15 16:14:50 +0330

    style(autocmds): put each event on a separate line

--- lua/sentiment/autocmds.lua
+++ lua/sentiment/autocmds.lua
@@ -4,11 +4,12 @@ local ui = require("sentiment.ui")
 local M = {}
 
 ---`Pair` renderer.
-M.renderer = Autocmd.new(
-  "renderer",
-  "Render pair",
-  { "BufWinEnter", "WinScrolled", "ModeChanged", "CursorMoved", "CursorMovedI" },
-  function() ui.render() end
-)
+M.renderer = Autocmd.new("renderer", "Render pair", {
+  "BufWinEnter",
+  "WinScrolled",
+  "ModeChanged",
+  "CursorMoved",
+  "CursorMovedI",
+}, function() ui.render() end)
 
 return M
