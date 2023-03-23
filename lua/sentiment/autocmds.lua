88a2ebe6ee81c4fcc83b81240752ca833c4e2d75
Amirabbas Fazelinia <utilyre@proton.me>
2023-03-23 16:55:19 +0330

    refactor(autocmds): rename `renderer_timer` to `timer`

--- lua/sentiment/autocmds.lua
+++ lua/sentiment/autocmds.lua
@@ -3,7 +3,7 @@ local ui = require("sentiment.ui")
 
 local M = {}
 
-local renderer_timer = nil
+local timer = nil
 
 local renderer = Autocmd.new({
   name = "renderer",
@@ -15,7 +15,7 @@ local renderer = Autocmd.new({
     "CursorMoved",
     "CursorMovedI",
   },
-  callback = function() renderer_timer = ui.render() end,
+  callback = function() timer = ui.render() end,
 })
 
 ---Start rendering pairs.
@@ -23,7 +23,7 @@ function M.start_rendering()
   if renderer:exists() then return end
 
   for _, win in ipairs(vim.api.nvim_list_wins()) do
-    renderer_timer = ui.render(win)
+    timer = ui.render(win)
   end
 
   renderer:create()
@@ -34,9 +34,7 @@ function M.stop_rendering()
   if not renderer:exists() then return end
 
   renderer:remove()
-  if renderer_timer ~= nil and renderer_timer:is_active() then
-    renderer_timer:close()
-  end
+  if timer ~= nil and timer:is_active() then timer:close() end
 
   for _, buf in ipairs(vim.api.nvim_list_bufs()) do
     ui.clear(buf)
